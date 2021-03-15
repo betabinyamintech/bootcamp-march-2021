6181db8ff716724d0226915c8a61a4044afe6964
Tamar Levanoni <tamari250@gmail.com>
2021-03-15 11:57:09 +0200

    add get methods

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +40,23 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+const usersSchema = new mongoose.Schema({
+  user_name: String,
+  password: String,
+});
+const User = mongoose.model("User", usersSchema);
+
+app.get("/login/:user_name/:password", async(req, res) => {
+  const { user_name,password } = req.params;
+  const user = await User.findOne({user_name,password}).exec();
+  res.send(user ?? {});
+});
+app.get("/register/:user_name/:password", async(req, res) => {
+  const { user_name,password } = req.params;
+  const user=await new User({user_name,password }).save();
+  res.send(user ?? {});
+});
+
 
 module.exports = app;
