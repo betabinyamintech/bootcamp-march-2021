a85b476bd6611b6b89fbf5156eed5f8f8250395b
ozi380 <ozi380@gmail.com>
2021-03-15 18:48:01 +0200

    add profile edit component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import LoginRegister from "./components/Login/LoginRegister";
+import ProfileEdit from "./components/ProfileEdit/ProfileEdit";
 
 const App = () => {
-  return <LoginRegister />;
+  return <ProfileEdit />;
 };
 
 export default App;
