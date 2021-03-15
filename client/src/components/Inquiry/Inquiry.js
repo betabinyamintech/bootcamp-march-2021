6dc7f15d01d3f535a063e25da8714210646696bc
elior-gispan <eliorg82@gmail.com>
2021-03-15 21:26:12 +0200

    css and inquiry properties updates

--- client/src/components/Inquiry/Inquiry.js
+++ client/src/components/Inquiry/Inquiry.js
@@ -8,7 +8,7 @@ const Inquiry = ({ inquiry }) => {
       <div className="inquiryBox">
         <div className="inquiryTitle">{inquiryTitle}</div>
         <div className="timePassed">{timePassed}</div>
-        <div className="statusMessage">{statusMessage}</div>
+        <div className="statusMessage">&bull; {statusMessage}</div>
         <button className="nextStepButton">בחירת מומחה &nbsp;&nbsp;&gt;</button>
       </div>
     </div>
