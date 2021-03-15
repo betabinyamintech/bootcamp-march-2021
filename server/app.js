e313d391355cb6032f3d72bd18a64f1fcbe0be86
adir abargil <33230109+adir1661@users.noreply.github.com>
2021-03-15 19:35:42 +0200

    Revert "Server login to merge test "

--- server/app.js
+++ server/app.js
@@ -1,112 +1,41 @@
-require("dotenv").config();
-var createError = require("http-errors");
-var express = require("express");
-const bcrypt = require("bcrypt");
-const { authenticateToken, generateAccessToken } = require("./jwt");
-var logger = require("morgan");
-const {
-  connectDb,
-  models: { User },
-} = require("./models");
-connectDb();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-const salt = 10;
 
-app.use(logger("dev"));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (err, req, res, next) {
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
-});
-
-// app.post("/login", async (req, res) => {
-//   const { email, password} = req.body;
-//   const user = await User.findOne({ email,password }).exec();
-//   res.send(user);
-// });
-// app.post("/register", async (req, res) => {
-//   const { email, password,firstName,lastName,profession,phone,city,isExpert,expertDetails:{helpKind,inquirySubjects,questionsBeforeMeeting,lengthMeeting,preferredMeetingType,meetingAddress}} = req.body;
-//   console.log("email, password: ", email, password);
-//   const user = await new User({email, password,firstName,lastName,profession,phone,city,isExpert,expertDetails:{helpKind,inquirySubjects,questionsBeforeMeeting,lengthMeeting,preferredMeetingType,meetingAddress}}).save();
-//   res.send(user);
-// });
-app.post("/register", async (req, res) => {
-  try {
-    const {
-      email,
-      password,
-      firstName,
-      lastName,
-      profession,
-      phone,
-      city,
-      isExpert,
-      expertDetails: {
-        helpKind,
-        inquirySubjects,
-        questionsBeforeMeeting,
-        lengthMeeting,
-        preferredMeetingType,
-        meetingAddress,
-      },
-    } = req.body;
-    const hash = bcrypt.hashSync(password, salt);
-    const token = generateAccessToken( {email });
-    console.log("token-------------------------:", token);
-    const user = await new User({
-      email,
-      password: hash,
-      firstName,
-      lastName,
-      profession,
-      phone,
-      city,
-      isExpert,
-      expertDetails: {
-        helpKind,
-        inquirySubjects,
-        questionsBeforeMeeting,
-        lengthMeeting,
-        preferredMeetingType,
-        meetingAddress,
-      },
-    }).save();
-    console.log(user);
-    const objectUser = user.toObject();
-    objectUser.token = token;
-    res.send(objectUser);
-  } catch (err) {
-    res.send(err.message);
-  }
-});
-
-app.post("/login",authenticateToken, async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email }).exec();
-  const token = generateAccessToken({ email });
-
-  const isValid = bcrypt.compareSync(password, user.password);
-  if (!isValid) throw Error("user not valid");
-  const objectUser = user.toObject();
-  objectUser.token = token;
-  res.send(objectUser);
-});
-app.get('/hi', authenticateToken,(req,res)=>{
-  res.send('hello awsome team number 1!');
-})
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-app.listen(process.env.PORT, () => {
-  console.log("Opened port succesfully at port " + process.env.PORT);
+  res.render('error');
 });
 
 module.exports = app;
