45a5738362884ac51b8676071985b771d028b3d5
hilelley <67314778+hilelley@users.noreply.github.com>
2021-03-15 15:08:19 +0200

    jwt

--- server/app.js
+++ server/app.js
@@ -1,44 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const mongoose = require("mongoose");
+const authenticateToken = require("./jwt");
+const dotenv = require("dotenv");
+dotenv.config();
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 const usersSchema = new mongoose.Schema({
@@ -47,16 +52,20 @@ const usersSchema = new mongoose.Schema({
 });
 const User = mongoose.model("User", usersSchema);
 
-app.get("/login/:user_name/:password", async(req, res) => {
-  const { user_name,password } = req.params;
-  const user = await User.findOne({user_name,password}).exec();
-  res.send(user ?? {});
+app.post("/login", async (req, res) => {
+  const { user_name, password } = req.body;
+  const user = await User.findOne({ user_name }).exec();
+  const isValid = bcrypt.compareSync(password, user.hash);
+  if (!isValid) throw Error("user not valid");
+  res.send(user);
 });
-app.get("/register/:user_name/:password", async(req, res) => {
-  const { user_name,password } = req.params;
-  const user=await new User({user_name,password }).save();
-  res.send(user ?? {});
+app.post("/register", async (req, res) => {
+  const { user_name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  const token = generateAccessToken({ user_name, hash });
+  const user = await new User({ user_name, hash }).save();
+  user.token = token;
+  res.send(user);
 });
-
 
 module.exports = app;
