fd898a83442cfe44e7a08628283faa89df813bb0
Tamar Levanoni <tamari250@gmail.com>
2021-03-15 15:05:55 +0200

    connect to db

--- server/app.js
+++ server/app.js
@@ -1,58 +1,60 @@
 require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
+// var path = require("path");
 var logger = require("morgan");
-const  connectDb  = require("./models");
+const  {connectDb,models:{User}}  = require("./models");
 connectDb();
 
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 app.get("/login/:user_name/:password", async (req, res) => {
   const { user_name, password } = req.params;
   const user = await User.findOne({ user_name, password }).exec();
   res.send(user ?? {});
 });
-app.get("/register/:user_name/:password", async (req, res) => {
-  const { user_name, password } = req.params;
-  const user = await new User({ user_name, password }).save();
-  res.send(user ?? {});
+app.get("/users", async (req, res) => {
+  const users = await User.find({}).exec();
+  res.send(users ?? {});
 });
 
+app.post("/register", async(req, res) => {
+  const { username, password } = req.body;
+const user= await new User({ username, password }).save();
+
+  res.send(user);
+});
+app.post("/login", async(req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, password }).exec();
+  res.send(user ?? {});
+});
+// app.get("*", async (req, res) => {
+//   console.log("123");
+//   res.send('ok');
+
+//   // const { user_name, password } = req.params;
+//   // const user = await User.findOne({ user_name, password }).exec();
+//   // res.send(user ?? {});
+// });
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 app.listen(process.env.PORT, () => {
   console.log("Opened port succesfully at port " + process.env.PORT);
 });
--- server/models/index.js
+++ server/models/index.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const User = require("./user");
 
-const connectDb = () => {
+const connectDb = async () => {
   const mongoUrl = process.env.MONGO_USER_NAME_PASS;
   console.log("Connecting to mongo server: " + mongoUrl);
-  mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+  return await mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 };
 const models = { User };
 
-module.exports =  connectDb;
+module.exports = {connectDb,models};
