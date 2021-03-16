bc4b3583969780b8715aa5d23e159707729ce27e
adir abargil <adir1661@users.noreply.github.com>
2021-03-16 17:58:50 +0200

    context reducer

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import Button from '../Common/Button/Button';
 import InputField from '../Common/InputField/InputField';
+import LoginRegister from '../Login/LoginRegister';
 import './Test.css'
+import {useUserState} from '../../contexts/UserContext'
 
-function Test(){
-    const [name,setName] = useState('adir')
+function Test() {
+    const [user,setUser] = useUserState();
     return <div>
-        <Button onClick={()=>setName('yonatan')}> {name}</Button>
-        <InputField label="hello"/>
-        </div>
-    }
+        {JSON.stringify(user)}
+        <LoginRegister onLogin={setUser}/>
+    </div>
+}
 export default Test;
