4d19c48a3d36806c62888da06896672dd13faf98
aviel-lanzman <avielnzmn@gmail.com>
2021-03-17 22:21:52 +0200

    fix context useEffect

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,22 +1,22 @@
+import { useEffect } from "react";
 import { refreshUserByToken } from "../../contexts/actions";
 import { useUserDispatch, useUserState } from "../../contexts/context";
-import LoginRegister from "../Login/LoginRegister";
+// import LoginRegister from "../Login/LoginRegister";
 import MainRouter from "../MainRouter";
 
 import "./Test.css";
 
 function Test() {
-    const userState =  useUserState()
-    console.log('userState', userState);
-    const userDispatch = useUserDispatch();
-    if (userState.user == null){
-        refreshUserByToken(userDispatch) 
+  const userState = useUserState();
+  const userDispatch = useUserDispatch();
+  useEffect(() => {
+    console.log("userState", userState);
+    if (userState.user == null) {
+      refreshUserByToken(userDispatch);
     }
-  
-//   return <LoginRegister />;
-return <MainRouter/>
+  });
+
+  return <MainRouter />;
 }
 
 export default Test;
-//<ProfileView />
-//<UserProfileEdit />
