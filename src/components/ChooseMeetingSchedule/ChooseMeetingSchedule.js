c6e52957510b553643d97a0a2d3e3d6c519b742d
HavaElboim <h.elboim@gmail.com>
2021-03-17 22:00:50 +0200

    Choose meeting schedule screen

--- src/components/ChooseMeetingSchedule/ChooseMeetingSchedule.js
+++ src/components/ChooseMeetingSchedule/ChooseMeetingSchedule.js
@@ -1,7 +1,45 @@
 import React from "react";
+import OneMeetingDetailsDisplay from "./OneMeetingDetailsDisplay";
+import ExpertDetailsHeader from "./ExpertDetailsHeader";
+
+// user is offered 3 dates, each is a string in format: 03/10/2021 12:00 AM
+
+let d = new Date();
+const arrayOf3DateTimes = [
+  "03/10/2021 12:00 PM",
+  "04/10/2021 10:00 AM",
+  "05/12/2021 08:00 AM",
+];
+
+const expert = {
+  firstName: "אליה",
+  lastName: "לוי",
+  profession: "רופא שיניים",
+  city: "ירושלים",
+  meetingPlace: "שער בנימין",
+  meetingTime: "30 דקות",
+};
 
 const ChooseMeetingSchedule = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>בחירת מועד לפגישה</h1>
+      <h2>{expert.meetingTime} פגישה</h2>
+      <ExpertDetailsHeader expert={expert} />
+      {arrayOf3DateTimes.map((dateTime) => (
+        <OneMeetingDetailsDisplay dateTime={dateTime} />
+      ))}
+    </div>
+  );
 };
+
+/* {arrayOf3DateTimes.map(({ date, time }) => (
+        <OneMeetingDetailsDisplay date={date} time={time} />
+      ))} */
+/* 
+   {hashtagsWithState.map(({ title, active }) => (
+        <Hashtag
+          onClick={() => {
+*/
 
 export default ChooseMeetingSchedule;
--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,14 +1,35 @@
-import { useUserState } from "../../contexts/context";
-import LoginDetails from "../Login/LoginDetails";
-import LoginRegister from "../Login/LoginRegister";
-import "../Login/Style.css";
-import MeetingArrangment from "../MeetingArrangment/MeetingArrangment";
-import MainRouter from "../../MainRouter";
+// import { useUserState } from "../../contexts/context";
+// import LoginDetails from "../Login/LoginDetails";
+// import LoginRegister from "../Login/LoginRegister";
+// import "../Login/Style.css";
+// import MeetingArrangment from "../MeetingArrangment/MeetingArrangment";
+import ChooseMeetingSchedule from "../ChooseMeetingSchedule/ChooseMeetingSchedule";
 
 import "./Test.css";
 
+// const arrayOf3DateTimes = [
+//   { date: Date.now(), time: "12:00" },
+//   { date: Date.now(), time: "14:00" },
+//   { date: Date.now(), time: "09:00" },
+// ];
+// const expert = {
+//   firstName: "אליה",
+//   lastName: "לוי",
+//   profession: "רופא שיניים",
+//   city: "ירושלים",
+//   meetingPlace: "שער בנימין",
+// };
+
 function Test() {
-  return <MainRouter />;
+  return (
+    <div>
+      <ChooseMeetingSchedule />
+    </div>
+  );
+}
+// <ChooseMeetingSchedule />
+{
+  /* <MeetingArrangment /> */
 }
 
 export default Test;
