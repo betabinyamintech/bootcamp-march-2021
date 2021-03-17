3e4d5cd129e616f3e6a67a3bb57251200b491cbd
HavaElboim <h.elboim@gmail.com>
2021-03-17 10:47:45 +0200

    started search for experts screen

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,6 +1,15 @@
-import MeetingScheduled from "../meetingScheduled/meetingScheduled";
+import SearchForExpert from "../SearchForExpert/SearchForExpert";
 import "./Test.css";
 
-const Test = () => <MeetingScheduled />;
+const requestDetails = {
+  questionText: "איך אני מחזיר את העסק שלי לאיזון?",
+  labelText: "בחירת מומחה לסיוע בשאלה:",
+};
+const Test = () => (
+  <SearchForExpert
+    questionText={requestDetails.questionText}
+    labelText={requestDetails.labelText}
+  />
+);
 
 export default Test;
