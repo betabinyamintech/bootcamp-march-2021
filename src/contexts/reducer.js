2e512c9bd4edddcb4e4f34bc1e71c1a1aaf0a46d
ozi380 <ozi380@gmail.com>
2021-03-17 20:37:02 +0200

    fix reducer

--- src/contexts/reducer.js
+++ src/contexts/reducer.js
@@ -1,21 +1,21 @@
-
-
 export const initialState = {
-    user: null
+  user: {
+    name: "Elior",
+  },
 };
 
 export function reducer(state = initialState, action) {
-    switch (action.type) {
-        case 'REQUEST_LOGIN':
-            return { ...state, loading: true}
-        case 'LOGIN_ERROR':
-            return { ...state, error: action.error, loading: false }
-        case 'LOGIN_SUCCESS':
-            return { ...state, user: action.user, loading: false };
-        case 'SET_inquries':
-            return { ...state, inquries: action.inquries };
-        default:
-            console.log('reducer: unknown type: ' + action.type)
-        //throw new Error();
-    }
+  switch (action.type) {
+    case "REQUEST_LOGIN":
+      return { ...state, loading: true };
+    case "LOGIN_ERROR":
+      return { ...state, error: action.error, loading: false };
+    case "LOGIN_SUCCESS":
+      return { ...state, user: action.user, loading: false };
+    case "SET_inquries":
+      return { ...state, inquries: action.inquries };
+    default:
+      console.log("reducer: unknown type: " + action.type);
+    //throw new Error();
+  }
 }
