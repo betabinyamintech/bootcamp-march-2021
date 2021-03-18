d72e0b5d4092c88aee0c94544c1d006e7648cd51
HavaElboim <h.elboim@gmail.com>
2021-03-18 13:14:53 +0200

    changed data format of user/expert to that in server

--- src/components/ChooseMeetingSchedule/ChooseMeetingSchedule.js
+++ src/components/ChooseMeetingSchedule/ChooseMeetingSchedule.js
@@ -5,6 +5,7 @@ import PreviousButton from "../Common/PreviousButton/PreviousButton";
 import tickForButton from "../Common/tickForButton.svg";
 
 import Button from "../Common/Button/Button";
+import experts from "./experts.json";
 
 // user is offered 3 dates, each is a string in format: 03/10/2021 12:00 AM
 
@@ -15,22 +16,45 @@ const arrayOf3DateTimes = [
   "05/12/2021 08:00 AM",
 ];
 
+
+
 const expert = {
   firstName: "אליה",
   lastName: "לוי",
   profession: "רופא שיניים",
   city: "ירושלים",
-  meetingPlace: "zoom",
-  meetingTime: "30 דקות",
+  expertDetails.meetingPlace: "zoom",
+  expertDetails.lengthMeeting: "30 דקות",
   Avatar:
     "https://thumbnail.imgbin.com/17/14/1/imgbin-user-ms-attendant-avatar-vRH5HKwaP0G2ABiJFCqhiGjT5_t.jpg",
 };
+/* schema of user/expert :
+
+const userSchema = new mongoose.Schema(
+  {
+    ...
+    firstName: String,
+    lastName: String,
+    profession: String,
+    city: String,
+    imageSrc: String,
+    ...
+expertDetails: {
+      isVerified: Boolean,
+      helpKind: String,
+      inquiryTags: [String],
+      questionsBeforeMeeting: [String],
+      lengthMeeting: Number,
+      preferredMeetingType: { type: String, enum: ["physically", "virtual"] },
+      meetingAddress: String,
+    },
+    */
 
 const ChooseMeetingSchedule = () => {
   return (
     <div>
       <div className="pageTitle">בחירת מועד לפגישה</div>
-      <div className="subTitle">{expert.meetingTime} פגישה</div>
+      <div className="subTitle">{expert.expertDetails.lengthMeeting} פגישה</div>
       <ExpertDetailsHeader expert={expert} />
       {arrayOf3DateTimes.map((dateTime) => (
         <OneMeetingDetailsDisplay dateTime={dateTime} />
