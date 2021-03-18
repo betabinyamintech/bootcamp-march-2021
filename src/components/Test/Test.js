555ea3f883b787fc5082d72feea323663b0199b0
elior-gispan <eliorg82@gmail.com>
2021-03-18 12:00:41 +0200

    fix

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,36 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { useEffect } from "react";
-import { refreshUserByToken } from "../../contexts/actions";
-import { useUserDispatch, useUserState } from "../../contexts/context";
-// import LoginRegister from "../Login/LoginRegister";
-import MainRouter from "../MainRouter";
-
-import "./Test.css";
-
-function Test() {
-  const userState = useUserState();
-  const userDispatch = useUserDispatch();
-  useEffect(() => {
-    console.log("userState", userState);
-    if (userState.user == null) {
-      refreshUserByToken(userDispatch);
-    }
-  });
-
-  return <MainRouter />;
-}
-=======
-import RequestStatus from "../Common/RequestStatusWindow/RequestStatus";
-import HashtagComponent from "../HashtagComponent/HashtagComponent";
-import "./Test.css";
-
-const Test = () => <HashtagComponent />;
->>>>>>> 798826f87d309acd54d8ee01253ba34f4cc7a5e8
-=======
 import "./Test.css";
 
 function Test() {}
->>>>>>> 333926e4a2ca678a538a89c540733b3eebbfd65a
 
 export default Test;
