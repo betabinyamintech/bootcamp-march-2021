84071ae49b330e514ac758f6b29ec12a9d867d14
elior-gispan <eliorg82@gmail.com>
2021-03-18 10:36:40 +0200

    fixing path of an import

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -7,15 +7,15 @@ import QuestionScreen from "../RequestStatusWindow/QuestionScreen";
 import "./Test.css";
 
 function Test() {
-    const userState =  useUserState()
-    console.log('userState', userState);
-    const userDispatch = useUserDispatch();
-    if (userState.user == null){
-        refreshUserByToken(userDispatch) 
-    }
-  
-//   return <LoginRegister />;
-return <MainRouter/>
+  const userState = useUserState();
+  console.log("userState", userState);
+  const userDispatch = useUserDispatch();
+  if (userState.user == null) {
+    refreshUserByToken(userDispatch);
+  }
+
+  //   return <LoginRegister />;
+  return <MainRouter />;
 }
 
 export default Test;
